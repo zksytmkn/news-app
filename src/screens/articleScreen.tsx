7e24e0fe6297056f62168170e714927028f8b23d
nakamoto <zksytmkn4869@gmail.com>
2023-11-21 23:34:16 +0900

    ✨

--- src/screens/articleScreen.tsx
+++ src/screens/articleScreen.tsx
@@ -1,7 +1,10 @@
 import { RouteProp } from '@react-navigation/native';
+import { useAtom } from 'jotai';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+import { ClipButton } from '../components/clipButton';
+import { clipAtom } from '../state/clip';
 import { RootStackParamList } from '../types/navigation';
 
 type Props = {
@@ -10,8 +13,26 @@ type Props = {
 
 export const ArticleScreen: React.FC<Props> = ({ route }: Props) => {
   const { article } = route.params;
+
+  const [clips, setClips] = useAtom(clipAtom);
+
+  const isClipped = () => {
+    return clips.some((clip) => clip.url === article.url);
+  };
+
+  const toggleClip = () => {
+    if (isClipped()) {
+      const newClips = clips.filter((clip) => clip.url !== article.url);
+      setClips(newClips);
+    } else {
+      const newClips = [...clips, article];
+      setClips(newClips);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <ClipButton onPress={toggleClip} enabled={isClipped()} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
@@ -21,5 +42,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-start',
   },
 });
--- src/screens/clipScreen.tsx
+++ src/screens/clipScreen.tsx
@@ -1,9 +1,32 @@
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useAtom } from 'jotai';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 
-export const ClipScreen = () => {
+import { ListItem } from '../components/listItem';
+import { clipAtom } from '../state/clip';
+import { RootStackParamList } from '../types/navigation';
+
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'Clip'>;
+};
+
+export const ClipScreen: React.FC<Props> = ({ navigation }: Props) => {
+  const [clips] = useAtom(clipAtom);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Clip Screen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate('Article', { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
@@ -12,5 +35,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-start',
   },
 });
