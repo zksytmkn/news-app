edc96c00d2c04de18279dae1dd3d6f0707cd3dda
nakamoto <zksytmkn4869@gmail.com>
2023-11-25 12:21:35 +0900

    ⚡️

--- src/state/clip.ts
+++ src/state/clip.ts
@@ -1,5 +1,8 @@
-import { atom } from 'jotai';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 
 import { Clip } from '../types/clip';
 
-export const clipAtom = atom<Clip>([]);
+const storage = createJSONStorage<any>(() => AsyncStorage);
+
+export const clipAtom = atomWithStorage<Clip>('clip', [], storage);
